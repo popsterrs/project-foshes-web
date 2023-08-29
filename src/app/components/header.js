462d8dd757a155ec7cf76a61e166a7773615fc8b
popsterrs <jackpadbury1@icloud.com>
2023-08-29 11:57:10 +0100

    fixed navbar on dynamic routed pages

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -11,10 +11,10 @@ export default function Header() {
                 </a>
 
                 <div className='header-navbar'>
-                    <a href='items'>Items</a>
-                    <a href='players'>Players</a>
-                    <a href='trading'>Trading</a>
-                    <a href='blogs'>Blogs</a>
+                    <a href='/items'>Items</a>
+                    <a href='/players'>Players</a>
+                    <a href='/trading'>Trading</a>
+                    <a href='/blogs'>Blogs</a>
                 </div>
 
                 <PrimaryButton content={<span>Sign Into Roblox</span>} className='header-sign-in'/>
