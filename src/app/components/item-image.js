e37e0105ce9a59bbedab26464c9f1e8bd6808abf
popsterrs <jackpadbury1@icloud.com>
2023-08-31 18:44:48 +0100

    revert nextjs image but keep name change to keep deployment working

--- src/app/components/item-image.js
+++ src/app/components/item-image.js
@@ -7,8 +7,8 @@ export default function Image(props) {
         const imageUrl = 'https://assetdelivery.roblox.com/v1/asset/?id=' + props.icon
 
         return (
-            // <img src={imageUrl} alt='item image'></img>  
-            <Image src={imageUrl} alt='item image'></Image>
+            <img src={imageUrl} alt='item image'></img>  
+            // <Image src={imageUrl} alt='item image'></Image>
         )
     } else {
         return (
