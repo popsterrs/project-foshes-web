7c54326c24b9dfd0927a1a141a7f369d207d1fc3
popsterrs <jackpadbury1@icloud.com>
2023-09-12 21:45:32 +0100

    Update data.js

--- src/app/api/data.js
+++ src/app/api/data.js
@@ -4,3 +4,8 @@ function GetItems() {
     return apiReturn
 }
 
+async function GetItemById(itemId) {
+    const items = await GetItems();
+
+    return items.find(item => item.id === Number(itemId));
+}
