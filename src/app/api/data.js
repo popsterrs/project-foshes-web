2a896bc6a658c76e80f805949d782d60696832ab
popsterrs <jackpadbury1@icloud.com>
2023-09-13 21:13:21 +0100

    Update data.js

--- src/app/api/data.js
+++ src/app/api/data.js
@@ -3,7 +3,7 @@ import apiReturn from 'src/app/api/apiReturn.json';
 function GetItems() {
     return apiReturn
 }
-
+// 
 async function GetItemById(itemId) {
     const items = await GetItems();
 
