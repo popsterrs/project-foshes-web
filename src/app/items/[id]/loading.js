398be4bd12d0cdae2f9bd9cc42abc089d67a27af
popsterrs <jackpadbury1@icloud.com>
2023-09-20 16:00:45 +0100

    Update loading.js

--- src/app/items/[id]/loading.js
+++ src/app/items/[id]/loading.js
@@ -1,12 +1,8 @@
 import 'src/app/styles/loading.css'
 import 'src/app/styles/item-page.css'
-import ItemImage from 'src/app/components/item-image.js';
 
 export default function Loading() {
-
-
     return (
-
         <div className='item-page-bounds'>
             <div className='loading-card'/>
             <div className='item-page-wrapper'>
