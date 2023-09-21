f869930d53759b698355c301e63ac655f3ff853e
popsterrs <jackpadbury1@icloud.com>
2023-09-21 10:55:19 +0100

    Update loading.js

--- src/app/items/[id]/loading.js
+++ src/app/items/[id]/loading.js
@@ -7,30 +7,17 @@ export default function Loading() {
             <div className='loading-card'/>
             <div className='item-page-wrapper'>
                 <div className='item-page-top'>
-                    <div className='item-page-left'>
-                        
-                    </div>
-
+                    <div className='item-page-left'/>
                     <div className='item-page-right'>
-                        <div className='item-page-right-row'>
-                        
-                        </div>
-
-                        <div className='item-page-right-row'>
-                            
-                        </div>
-
-                        <div className='item-page-right-row'>
-                            
-                        </div>
+                        <div className='item-page-right-row'/>
+                        <div className='item-page-right-row'/>
+                        <div className='item-page-right-row'/>
                     </div>
                 </div>
                 <div className='item-page-bottom'>
                     <h1>Sales History</h1>
 
-                    <div className='item-page-sales-chart'>
-                        
-                    </div>
+                    <div className='item-page-sales-chart'/>
                 </div>
             </div>
         </div>
