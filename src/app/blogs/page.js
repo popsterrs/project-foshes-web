54e744f55f4d135ccd78e07ce54bddd300f5fd31
popsterrs <jackpadbury1@icloud.com>
2023-09-24 16:01:09 +0100

    Update page.js

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -3,7 +3,14 @@ import 'src/app/styles/blogs.css'
 export default function Blogs() {
     return (
         <div className='blogs-bounds'>
-            blogs
-        </div>  
+            <div id="retainable-rss-embed"
+                data-rss="https://medium.com/feed/@username"
+                data-maxcols="3"
+                data-layout="slider"
+                data-poststyle="inline"
+                data-readmore="Read the rest"
+                data-buttonclass="btn btn-primary"
+                data-offset="-100"></div>
+        </div>
     )
 }
