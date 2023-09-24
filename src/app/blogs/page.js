7da9c98914d295df64f37fb037120e0ebc8215e7
popsterrs <jackpadbury1@icloud.com>
2023-09-24 16:14:51 +0100

    Update page.js

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,16 +1,35 @@
-import 'src/app/styles/blogs.css'
+'use client'
+import React, { useEffect } from 'react';
 
-export default function Blogs() {
-    return (
-        <div className='blogs-bounds'>
-            <div id="retainable-rss-embed"
-                data-rss="https://medium.com/feed/@username"
-                data-maxcols="3"
-                data-layout="slider"
-                data-poststyle="inline"
-                data-readmore="Read the rest"
-                data-buttonclass="btn btn-primary"
-                data-offset="-100"></div>
-        </div>
-    )
+function Blogs() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <div className='blogs-bounds'>
+      <div className='blogs-holder'>
+        <div
+          id="retainable-rss-embed"
+          data-rss="https://medium.com/feed/retainable,https://medium.com/feed/vue-mastery"
+          data-maxcols="3"
+          data-layout="grid"
+          data-poststyle="inline"
+          data-readmore="Read the rest"
+          data-buttonclass="btn btn-primary"
+          data-offset="-100"
+        ></div>
+      </div>
+    </div>
+  );
 }
+
+export default Blogs;
